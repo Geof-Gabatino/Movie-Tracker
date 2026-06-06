222019143500df50f028739a6fa814a27fc609ad
Geof Gabatino <geofcarino09@gmail.com>
2026-06-06 16:31:42 +0800

    Added renz pogi and sample initialization

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
+
 int main() {
     cout << "Hello, World!" << endl;
+    cout << "renz Pogi" << endl;
+    cout << "this is a sample initialization" << endl
+
     return 0;
-}
+    
