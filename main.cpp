8fb3d65c58ee310e8ecc298e0a4f498ce3d6e067
Geof-Gabatino <geofcarino09@gmail.com>
2026-06-06 17:08:04 +0800

    Time 5:07

--- main.cpp
+++ main.cpp
@@ -4,10 +4,7 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     cout << "renz Pogi" << endl;
-    cout << "this is a sample initialization" << endl
-
-        // Added comment on github browser for testing only
-        // time is 4:39. This'll be an example on how to use git pull at VS Studio
-        
-    return 0;
+    cout << "this is a sample initialization" << endl;
+    cout << "Time is 5:07" endl;
+        return 0;
     
