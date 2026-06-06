4d85791dd8d5403c272928c62e815fd44a58bd8d
Geof Gabatino <geofcarino09@gmail.com>
2026-06-06 17:45:39 +0800

    hophop hooray

--- main.cpp
+++ main.cpp
@@ -7,5 +7,17 @@ int main() {
     cout << "Test line 3" << endl;
 
     cout << "Test push to github" << endl;
-        return 0;
+
+    // start coding after initiating the git pull command to ensure the local repository is up to date with the remote repository before making any changes.
+
+    cout << "Test line 4" << endl;
+    cout << "Test line 5" << endl;
+    cout << "Test line 6" << endl;
+    cout << "Test line 7" << endl;
+    cout << "Test line 8" << endl;
+    cout << "Test line 9" << endl;
+    cout << "Test line 10" << endl;
+
+    return 0;
+}
     
